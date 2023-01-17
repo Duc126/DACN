cdf52915b57e31fa31399493f1516882ddde6bb9
Bui Dinh Duc <ducbui1211@gmail.com>
2023-01-17 16:37:19 +0700

    Update

--- modules/Report/Views/admin/statistic/index.blade.php
+++ modules/Report/Views/admin/statistic/index.blade.php
@@ -9,7 +9,7 @@
 @section ('content')
     <div class="container-fluid">
         <div class="d-flex justify-content-between mb20">
-            <h1 class="title-bar">{{__('Bookings Statistic')}}</h1>
+            <h1 class="title-bar">{{__('Statistic')}}</h1>
         </div>
         @include('admin.message')
         <div class="bravo-statistic">
